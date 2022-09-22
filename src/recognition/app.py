cd7589006647419a7d79fe1ceaa66d1a3babf793
romanzoniit <romanzoniit@gmail.com>
2022-09-22 11:49:11 +0300

    add new post request

--- src/recognition/app.py
+++ src/recognition/app.py
@@ -1,7 +1,10 @@
 import os.path
+
+import rarfile
+
 from recognition.methods import *
 from recognition.settings import UPLOADED_FILES_PATH, MODELS_FILES_PATH
-from fastapi import FastAPI, Response, status, UploadFile, File
+from fastapi import FastAPI, Response, status, UploadFile, File, BackgroundTasks
 from typing import Optional
 from starlette.responses import FileResponse
 
@@ -40,7 +43,7 @@ async def recognize_zip_file(response: Response,
         zfile = None
         for f in os.scandir(UPLOADED_FILES_PATH + os.path.splitext(file_name)[0]):
             if f.is_file() and f.path.split('.')[-1].lower() == 'wav':
-                wav_parse(os.path.splitext(file_name)[0] + "/" + f.name, media_type, model, format_type)
+                wav_parse(os.path.splitext(file_name)[0] + "/" + f.name, media_type, model)
                 zipfiles(UPLOADED_FILES_PATH, os.path.splitext(f.name)[0], normalize_flag, file_name)
             zfile = save_all_zip(ARCHIVED_FILES_PATH+os.path.splitext(file_name)[0], f.name, normalize_flag, file_name)
         file_resp = (FileResponse(zfile, media_type="application/x-zip-compressed", filename=zfile))
@@ -62,7 +65,7 @@ async def recognize_file(response: Response,
         if media_type == "mp3":
             mp3_parse(normalize_flag, file_name, media_type, model)
         elif media_type == "wav":
-            wav_parse(file_name, media_type, model, format_type)
+            wav_parse(file_name, media_type, model)
         zfile = zipfiles(UPLOADED_FILES_PATH, file_name, normalize_flag, file_name)
         file_resp = (FileResponse(zfile, media_type="application/x-zip-compressed", filename=zfile))
         response.status_code = status.HTTP_200_OK
@@ -88,3 +91,36 @@ async def upload_file(
             'tag': tag,
             'file': file
             }
+
+
+@app.post("/api/uprecognize", tags=["Upload and recognize"], status_code=status.HTTP_200_OK)
+async def upload_recognize(
+        response: Response,
+        background_tasks: BackgroundTasks,
+        file: UploadFile = File(...),
+                        ):
+    full_name = split_filename(file)
+    if not is_archive_file(file):
+        return {'msg': 'File must be RAR or ZIP format'}
+    else:
+        await save_file_to_uploads(file, full_name)
+    if not os.path.exists(UPLOADED_FILES_PATH+os.path.splitext(full_name)[0]):
+        os.mkdir(UPLOADED_FILES_PATH+os.path.splitext(full_name)[0])
+    if os.path.exists(UPLOADED_FILES_PATH+full_name) and rarfile.is_rarfile(full_name):
+        unrar_files(UPLOADED_FILES_PATH+full_name)
+    elif os.path.exists(UPLOADED_FILES_PATH+full_name) and zipfile.is_zipfile(full_name):
+        unzip_files(UPLOADED_FILES_PATH+full_name)
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'msg': 'File not found'}
+    for f in os.scandir(UPLOADED_FILES_PATH + os.path.splitext(full_name)[0]):
+        if f.is_file() and f.path.split('.')[-1].lower() == 'wav':
+            wav_parse(os.path.splitext(full_name)[0] + "/" + f.name, "wav", model)
+            print(f"Recognize: {f.name}")
+    result_file = merge_json_file(os.path.splitext(full_name)[0])
+    background_tasks.add_task(delete_files,
+                              UPLOADED_FILES_PATH + os.path.splitext(full_name)[0],
+                              UPLOADED_FILES_PATH + full_name)
+    return FileResponse(result_file, media_type="json",
+                        filename=result_file,
+                        background=background_tasks)
